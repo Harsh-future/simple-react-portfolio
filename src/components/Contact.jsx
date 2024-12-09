1cedb1ed61b1fa77499992546758d5d497283d3f
Harsh-future <hchauhan@909te.ch>
2024-12-10 02:01:22 +0530

    Project Updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { FaInstagram } from "react-icons/fa";
-import { CiFacebook } from "react-icons/ci";
 import { CiLinkedin } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
 import { BsTwitterX } from "react-icons/bs";
 import { CgMail } from "react-icons/cg";
+import { SiLeetcode } from "react-icons/si";
+import { FiYoutube } from "react-icons/fi";
+
 
 
 const Contact = () => {
@@ -13,20 +15,17 @@ const Contact = () => {
       <div className="container contact" id="contact">
         <h1>CONTACT ME</h1>
         <div className="contact-icon"  data-aos="zoom-in-up" data-aos-duration="1000">
-          <a href="https://www.google.com" target="_blank" className="items">
-            <FaInstagram className="icons" />
+          <a href="https://github.com/Harsh-future" target="_blank" className="items">
+            <FaGithub className="icons" />
           </a>
-          <a href="https://www.google.com" target="_blank" className="items">
-            <CiFacebook className="icons" />
+          <a href="https://leetcode.com/u/Harsh-541/" target="_blank" className="items">
+            <SiLeetcode className="icons" />
           </a>
-          <a href="https://www.google.com" target="_blank" className="items">
+          <a href="https://www.linkedin.com/in/harsh-chauhan-dailylife-2021/" target="_blank" className="items">
             <CiLinkedin className="icons" />
           </a>
-          <a href="https://www.google.com" target="_blank" className="items">
-            <BsTwitterX className="icons" />
-          </a>
-          <a href="https://www.google.com" target="_blank" className="items">
-            <FaGithub className="icons" />
+          <a href="https://www.youtube.com/@HarshChauhan-i7s" target="_blank" className="items">
+            <FiYoutube className="icons" />
           </a>
           <a href="mailto:harshchauhan.go@gmail.com" target="_blank" className="items">
             <CgMail className="icons" />
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,13 +12,13 @@ const Projects = () => {
               <div key={data.id} className="my-4 col-md-4 col-sm-6 col-lg-3 mx-4"  data-aos="flip-right" data-aos-duration="1500">
                 <div className="card bg-dark text-light">
                   <div className="img d-flex justify-content-center align-item-center p-3">
-                    <img src={data.imageSrc} className="card-img-top" alt="..." />
+                    <img src={`/Images/${data.imageSrc}`} className="card-img-top" alt="..." />
                   </div>  
                   <div className="card-body text-center">
                     <h5 className="card-title">{data.title}</h5>
                     <p className="card-text">{data.description}</p>
-                    <a href={data.demo} className="btn btn-primary mx-2">Demo</a>
-                    <a href={data.source} className="btn btn-warning">Code</a>
+                    <a href={data.demo} className="btn btn-primary mx-2" target="_blank">Demo</a>
+                    <a href={data.source} className="btn btn-warning" target="_blank">Code</a>
                   </div>
                 </div>
               </div>
